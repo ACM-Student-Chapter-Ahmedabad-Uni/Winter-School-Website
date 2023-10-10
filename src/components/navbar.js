f7b8a326268f3699650045cc5dbd74ca54ef4cb7
jineetshah <jineet.s@ahduni.edu.in>
2023-10-10 14:50:57 +0530

    Res

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,21 +5,22 @@ import { Navbar } from 'react-bootstrap';
 
 function TextLinkExample() {
   return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">ACM Ahmedabad University </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#events">Events</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#team">Team</Nav.Link>
-            <Nav.Link href="#resources">Resources</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Navbar className="bg-body-tertiary" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">ACM Ahmedabad University</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+      <Nav>
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#events">Events</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <Nav.Link href="#team">Team</Nav.Link>
+        <Nav.Link href="#resources">Resources</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
   );
 }
 
