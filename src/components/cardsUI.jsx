52851a9f294d89ef9a5c7a3686a98767f5282782
Paridhi <wunderkerzenmenschen19@gmail.com>
2023-10-16 13:26:05 +0530

    Added: Card UI

---
+++ src/components/cardsUI.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import Card from './cardUI';
+import cardImage from '../images/Card-dummy.jpeg';
+
+class Cards extends Component{
+    render(){
+        return(
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                    <Card imgsrc={cardImage} title="card 1"/>
+                    </div>
+                    <div className="col-md-4">
+                    <Card imgsrc={cardImage} title="card 2"/>
+                    </div>
+                    <div className="col-md-4">
+                    <Card imgsrc={cardImage} title="card 3"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Cards;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import './navbar-style.css';
 
 function TextLinkExample() {
   return (
     <Navbar className="bg-body-tertiary" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">ACM Ahmedabad University</Navbar.Brand>
+    <Navbar.Brand href="#home">ACM x Ahmedabad University</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
       <Nav>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#events">Events</Nav.Link>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#team">Team</Nav.Link>
-        <Nav.Link href="#resources">Resources</Nav.Link>
+        <Nav.Link href="#home">HOME</Nav.Link>
+        <Nav.Link href="#about">ABOUT</Nav.Link>
+        <Nav.Link href="#events">EVENTS</Nav.Link>
+        <Nav.Link href="#team">TEAM</Nav.Link>
+        <Nav.Link href="#resources">RESOURCES</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
