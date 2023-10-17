5a7c7119f74e484dc346a0c31ebcb931122e0a0b
jineetshah <jineet.s@ahduni.edu.in>
2023-10-17 14:36:56 +0530

    AAAA

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import logo from '../images/logo.jpg'; // replace with the actual path to your logo
 
 function TextLinkExample() {
-  return (
-    <Navbar className="bg-body-tertiary" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">ACM Ahmedabad University</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-      <Nav>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#events">Events</Nav.Link>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#team">Team</Nav.Link>
-        <Nav.Link href="#resources">Resources</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+  const [isOpen, setIsOpen] = useState(false);
 
+  return (
+    <div>
+      <Navbar className="custom-navbar" expand="lg" >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo" width="250" height="100" className="d-inline-block align-top" /> {/* adjust width and height as needed */}
+            {' '}
+            ACM Ahmedabad University
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#events">Events</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#team">Team</Nav.Link>
+              <Nav.Link href="#resources">Resources</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+    </div>
   );
 }
 
