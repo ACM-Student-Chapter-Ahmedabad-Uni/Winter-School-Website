bdd3fac88c7fe218d43d4e1f8c8fa11c4c806b7e
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-10-18 14:31:55 +0530

    Time line added

--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -9,7 +9,7 @@ function TeamCard(props) {
                 className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20 h-32 w-32 object-cover" alt=""/>
             <p className="mb-2 font-bold">{name}</p>
             <p className="text-neutral-500 dark:text-neutral-300">
-                Senior developer
+                CP Coders
             </p>
         </div>
         </>
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,14 +1,16 @@
 import React from "react"
 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Schedule from "../components/schedule"
+import Navbar from '../components/navbar'
 
 const SchedulePage = () => (
-  <Layout>
+  <>
+    <Navbar />
     <Schedule />
-  </Layout>
+  </>
 )
 
 export const Head = () => <Seo title="Schedule" />
