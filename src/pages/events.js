228adb264b6943bd64a3146fbe42df0398b49335
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-10-19 13:56:57 +0530

    Update events.js

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,14 +2,14 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Schedule from "../components/schedule"
 
 const NotFoundPage = () => (
   <Layout>
-    <Schedule />
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />
+export const Head = () => <Seo title="Woopsy...." />
 
 export default NotFoundPage
