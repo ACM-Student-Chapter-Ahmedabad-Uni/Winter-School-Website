3859de537939328c81eb6478ce06471d2577b020
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-10-30 21:54:40 +0530

    Update team_data.js

--- static/data/team_data.js
+++ static/data/team_data.js
@@ -1,29 +1,34 @@
 export const data = {
     "Team": [
         {
+            "name": "Yatri Mehta",
+            "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1168585864780185600/03b7da35-0cc8-406d-9033-0132f5130c8c.png?ex=65524d65&is=653fd865&hm=512a396ae9a6d829320240ace6c9dd49243b43dfe7cf0c96c024fd45f8f389d3&",
+            "title": ""
+        },
+        {
             "name": "Paridhi Jain",
             "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1167787587809841152/99dd5230-e5c8-4a24-849c-fae5b81a33c3.png?ex=654f65f1&is=653cf0f1&hm=6d140c6c2d4f2f9479bddaf41350ee11470c93edf7bc8b103e35d9025ca1395b&",
-            "title": "Chair"
+            "title": ""
         },
         {
             "name": "Harsh Bhagat",
             "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1167017128239562792/1672798476873.png?ex=654c9865&is=653a2365&hm=3981c0ec3b763d2e405dda10d8e76341bd692ee2bb2ae710043f8480fb61a980&",
-            "title": "Secretary"
+            "title": ""
         },
         {
             "name": "Mihir Kotecha",
             "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1164469108809678848/370222606_1380071009577549_5520256599924713303_n.png?ex=6543535e&is=6530de5e&hm=28041b7de13d417eacba6837f991d6bf2f44137e9e789d7f2db64c8cf865307e&",
-            "title": "Investigation Lead"
+            "title": ""
         },
         {
             "name": "Parva Parmar",
             "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1166362413164134450/image.png?ex=654a36a5&is=6537c1a5&hm=f2f0c027c93b0d027adb36f8601b1c834f596badfd2f444ec47bfbbd9c9fdc88&",
-            "title": "Investigation Lead"
+            "title": ""
         }, 
         {
             "name": "Jineet Shah",
             "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1167018236601188372/WhatsApp_Image_2023-10-26_at_14.03.33_3d5aa30d.jpg?ex=654c996d&is=653a246d&hm=63d8d79a845dab41a8a926b545dc692b51de6323c651363ed63ff12a99ab2115&",
-            "title": "Lead Developer"
+            "title": ""
         },
         {
             "name": "Dhruvi Rajput",
