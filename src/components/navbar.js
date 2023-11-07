8edf5a8eed4638b43fe7f638ca0bffb9d22dc31d
jineetshah <jineet.s@ahduni.edu.in>
2023-11-07 15:38:07 +0530

    Not much

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,34 +2,37 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import { Card } from 'react-bootstrap'; // Import Card component
 import './navbar-style.css';
 
 function TextLinkExample() {
-  // const [isOpen, setIsOpen] = useState(false);
-
   return (
     <Navbar expand="lg">
-        <Navbar.Brand href="/" className="text-sm sm:text-base md:text-lg lg:text-xl font-bold text-black no-underline hover:underline">ACM x Ahmedabad University</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+      <Navbar.Brand href="/" className="text-sm sm:text-base md:text-lg lg:text-xl font-bold text-black no-underline hover:underline">
+        ACM x Ahmedabad University
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
         <Nav className='mr-6'>
-            <Nav.Link href="/schedule" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
+          <Nav.Link href="/schedule" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
             SCHEDULE
-            </Nav.Link>
-            <Nav.Link href="/speakers" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
+          </Nav.Link>
+          <Nav.Link href="/speakers" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
             SPEAKERS
-            </Nav.Link>
-            <Nav.Link href="/team" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
+          </Nav.Link>
+          <Nav.Link href="/team" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
             TEAM
-            </Nav.Link>
-            <Nav.Link href="/venue" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
+          </Nav.Link>
+          <Nav.Link href="/venue" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
             VENUE
-            </Nav.Link>
-            <Nav.Link href="/resources" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
+          </Nav.Link>
+          <Nav.Link href="/resources" className="text-base font-medium text-gray-700 hover:text-gray-900 ml-4">
             RESOURCES
-            </Nav.Link>
+          </Nav.Link>
         </Nav>
-        </Navbar.Collapse>
+      </Navbar.Collapse>
+
+      
     </Navbar>
   );
 }
