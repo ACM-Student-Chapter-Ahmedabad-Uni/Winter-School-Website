d87f46a0cf90b9ee73ef8fdf6e20e09abb096c26
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-11-09 10:54:52 +0530

    update: added speakers images in static

--- static/data/speakers_data.js
+++ static/data/speakers_data.js
@@ -2,29 +2,29 @@ export const data = {
   "Speakers": [
     {
       "name": "Sougata Mukherjea",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1169655575823532153/SM_1.jpg?ex=655631a4&is=6543bca4&hm=8d2a159a4cf069d4f564cda7fdd16d30aee3a2cf4ae0fb161faa91442541cde0&",
+      "imageUrl": "../images/sougata.jpg",
       "title": `
       Sougata Mukherjee is a seasoned professional with a rich background in technology. He serves as a Professor of Practice in Computer Technology at IIT Delhi, bringing over a decade of experience in Project Management and Strategy Formation from his time at IBM. With 15+ years in R&D across prominent tech companies, including IBM, BEA, Inktomi, and NEC Research, Sougata holds expertise in Analytics, AI, Visualization, and Cloud technologies. He is proficient in Java and Python, and has led pivotal projects at IBM. Noteworthy accolades include the title of IBM Master Inventor, boasting numerous US patents, and a research citation count exceeding 5000. Sougata is a PhD holder in Computer Science from Georgia Tech.`
     },
     {
       "name": "Seema Nagar",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1170234477075759104/Seema1.jpg?ex=65584cc9&is=6545d7c9&hm=2d46f37b267c0edc7be2af95c44986898d44be45a291fb5dea1d2c1568092d76&",
+      "imageUrl": "../images/seema.jpg",
       "title": `
       Seema Nagar is a distinguished staff research scientist at IBM Research India, with a remarkable fifteen-year tenure in computer science research. Her contributions are underscored by over 50 publications in esteemed conferences and journals, along with an impressive portfolio of 150+ filed patents. Named a master inventor for two consecutive terms, Seema has also played a pivotal role in reviewer committees for conferences like IJCAI, NAACL, and ACL. Her outstanding work on social network analysis and trustworthy AI has earned her several Research Awards, including three Outstanding Technical Achievement Awards at IBM Research India. A dedicated mentor, she actively guides numerous researchers and students in the lab. Seema holds B. Tech. and M. Tech. degrees in Computer Science and Engineering from IIT, Guwahati (2007) and IIT, Delhi (2011) respectively. Currently, she is pursuing a part-time PhD from IIIT, Guwahati.`
     },
     {
       "name": "Nitendra Rajput",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1164493408979652709/1638625012019.png?ex=654369ff&is=6530f4ff&hm=133dfb96c81ea14ae560ad7db8cf36affc8213fae5f81b58bc58ea4bc1f97229&",
+      "imageUrl": "../images/nitendra.png",
       "title": "Nitendra Rajput is an ACM Distinguished Scientist and IEEE Senior Member. Currently leading the AI Garage center at Mastercard, he focuses on tackling Machine Learning challenges within the Financial sector. Nitendra's current interests lie in anomaly detection, time-series modeling, information extraction, scalable machine learning, and temporal graph networks. He welcomes collaborations in these domains as the center expands. With 18 years at IBM Research and a stint as EVP and Head of Analytics at InfoEdge, Nitendra has deep expertise in various facets of machine learning, HCI, software engineering, and mobile sensing. He has an extensive publication record and holds over 50 granted patents. Nitendra is also a notable figure in the conference circuit, serving on program committees and delivering tutorials at esteemed ACM venues."
     },
     {
       "name": "Ramasuri Narayanam",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1169655462489231371/Ramasuri.Narayanam.png?ex=65563189&is=6543bc89&hm=7650361f19a20594d7fa2642e23f668d18eb27cb10919f7db885757999097c3e&",
+      "imageUrl": "../images/ramasuri.png",
       "title": "Ramasuri Narayani is a Senior Research Scientist at Adobe Research, bringing over a decade of expertise in Artificial Intelligence (AI) solutions. Previously at IBM Research – India, he held the title of IBM Master Inventor, showcasing his proficiency in developing innovative analytics solutions across various sectors including telcos, retail, and technology domains like social media and business workflow analytics. Ramasuri boasts an extensive IP portfolio, with over 30 peer-reviewed research articles and 35+ granted patents. Recognized as an IEEE Senior Member and ACM Distinguished Speaker, he has received accolades, including the Best Ph.D. Thesis award from the Computer Society of India. Ramasuri actively contributes to conferences like IJCAI, AAAI, WWW, and CIKM, and enjoys pursuits like driving, traveling, and exploring new places."
     },
     {
       "name": "M.S. Santhanam",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1164494712506417172/5d8c1357-f6e3-4124-8f08-c20c1d5efbc7.png?ex=65436b36&is=6530f636&hm=4a8e376272b61e56c52d6c3ee51da22f32970833f591fa4f8b75af65244dae3d&https://example.com/nitendra.jpg",
+      "imageUrl": "../images/santhanam.png",
       "title": `M S Santhanam is a distinguished professor of physics at the Indian Institute of Science Education and Research (IISER) in Pune, India. His research focus encompasses a diverse range of fields including nonlinear dynamics, quantum computing, quantum chaos, statistical physics, complex systems, and extreme events. Additionally, he is an active member of the I-Hub Quantum Technology Foundation, hosted at IISER Pune. Beyond academia, M S Santhanam holds a keen interest in Indian politics, science and technology matters, as well as higher education. In his free time, he engages in writing popular articles for leading newspapers and magazines, contributing insights on science and various topics of personal interest.`
     },
     // {
@@ -35,17 +35,17 @@ export const data = {
     // },
     {
       "name": "Sitabhra Sinha",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1165650350603247616/sitabhra-sinha.png?ex=65479f7c&is=65352a7c&hm=b6ac49d8f27f14c08acecb24be8258fd9f62119c84dd742e4070105878973295&",
+      "imageUrl": "../images/sitabhra.png",
       "title": `Sitabhra Sinha is a distinguished Professor in the Physics group at the Institute of Mathematical Sciences (IMSc) in Chennai, and also serves as adjunct faculty at the National Institute of Advanced Studies (NIAS) in Bangalore. He obtained his Ph.D. from the Machine Intelligence Unit at the Indian Statistical Institute, Calcutta, focusing on the nonlinear dynamics of recurrent neural network models. Dr. Sinha's research spans complex systems, nonlinear dynamics, and theoretical & computational biophysics. His current work delves into diverse areas including the physics of cardiac arrhythmias, network dynamics, systems biology, computational neuroscience, and the physics of social and economic phenomena. With an extensive academic journey, he has contributed significantly to understanding emergent phenomena in various complex systems.`
     },
     {
       "name": "Loyimee Gogoi",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1172037136879472710/Loyimee_gogoi.JPG?ex=655edba4&is=654c66a4&hm=e627190ba7ce1645143e7916e2a82e33243c93e8a2d71397ead497a9a9af09d7&",
+      "imageUrl": "../images/loyimee.jpg",
       "title": "Loyimee Gogoi is an Assistant Professor at Amrut Mody School of Management, Ahmedabad University. She earned her MSc in Mathematics (Tezpur University), and PhD in Mathematics (Dibrugarh University). She was a researcher at Northwestern Polytechnical University, Xi’an, China from September 2019 to August 2021. She worked as a researcher in other institutes like Queen’s University Belfast (QUB), the UK and Indian Statistical Institute, Kolkata. Before joining Ahmedabad University, she was an Assistant Professor at Xavier's Institute of Management and Entrepreneurship, Chennai. Professor Gogoi has four years of experience in research. Her research area is Cooperative Game Theory and its applications in networks. She has several publications at European Journal of Operational Research, Operational Research, TOP, Mathematical Social Sciences, and International Journal of Economic Theory etc."
     },
     {
       "name": "Amit Nanavati",
-      "imageUrl": "https://cdn.discordapp.com/attachments/1164468767175233536/1164469844834533386/300_x_400_amit_nanavati-1.png?ex=6543540d&is=6530df0d&hm=acb46162effb7cb04791abbd7782e4875699e7870ecd68331c910df050d33b27&",
+      "imageUrl": "../images/amit.png",
       "title": 'Amit A. Nanavati is a Professor at Ahmedabad University and an Adjunct Professor at IIT Delhi. Prior to this, Amit was a Senior Technical Lead for Cognitive Automation Solutions at IBM GTS Innovation Labs (2016-21), and a part of IBM Research (2000-16). He is very interested in trying to get insights from data, especially using Network Science.  He co-authored a book on "Speech in Mobile and Pervasive Environments" published by John Wiley, UK in 2012.  He became an ACM Distinguished Speaker in 2014, and an ACM Distinguished Scientist in 2015.'
     }
   ]
