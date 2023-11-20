58609e462e5fd2ec04899aeb26cfe86d16ddff9d
habhagat2003 <habhagat2003@gmail.com>
2023-11-20 10:18:21 +0000

    update: changes in the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,35 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { inject } from '@vercel/analytics';
 
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`)
 
-export const Head = () => <Seo title="Woopsy...." />
+  const siteUrl = data.site.siteMetadata.siteUrl
+  inject();
+  return <>
+    <h1>Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <p>Here are some things you can do:</p>
+    <ul>
+      <li>Check the URL to make sure it is correct.</li>
+      <li>Try searching for the page using the search bar at the top of the page.</li>
+      <li>Go back to the main site by clicking the link below.</li>
+    </ul>
+    <a href={`${siteUrl}/`}>Go back to the main site</a>
+  </>
+}
+
+export const Head = () => <Seo title="404 Error" />
 
 export default NotFoundPage
