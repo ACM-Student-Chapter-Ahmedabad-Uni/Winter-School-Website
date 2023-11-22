166d1a2ac7e76467fc9be89f7fd8ccc7b8730303
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-11-22 19:26:18 +0530

    update: schedule UI changes

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,54 +1,30 @@
 import React from "react";
-// import {
-//     Accordion,
-//     AccordionHeader,
-//     AccordionBody,
-//     Timeline,
-//     TimelineItem,
-//     TimelineConnector,
-//     TimelineIcon,
-//     Typography,
-//     TimelineHeader,
-// } from "@material-tailwind/react";
-// import { data } from "../../static/data/schedule_data.js"
+import { data } from "../../static/data/schedule_data.js"
 
 export function AccordionCustomStyles() {
-    
-    // const [open, setOpen] = React.useState(0);
-
-    // const handleOpen = (value) => setOpen(open === value ? 0 : value);
-
     return (
-        <>
-            {/* {data.Days.map((day, index) => (
-                <Accordion id={`day${index + 1}`} onClick={() => handleOpen(index + 1)} open={open === index + 1} className="mb-2 rounded-lg border border-blue-gray-100 px-4" key={index}>
-                    <AccordionHeader
-                        className={`border-b-0 transition-colors ${open === index + 1 ? "text-blue-500 hover:!text-blue-700" : ""
-                            }`}
-                    >
-                        {day.Day}
-                    </AccordionHeader>
-                    <AccordionBody className="pt-0 text-base font-normal">
-                        <Timeline>
-                            {day.Schedule.map((event, eventIndex) => (
-                                <TimelineItem className="md:h-28" key={eventIndex}>
-                                    <TimelineConnector className="md:!w-[78px]" />
-                                    <TimelineHeader className="relative rounded-xl border border-blue-gray-50 py-3 pl-4 pr-8 shadow-lg shadow-blue-gray-900/5">
-                                        <TimelineIcon className="p-3" variant="ghost">
-                                            <p className="text-sm md:text-base lg:text-lg">{event.time}</p>
-                                        </TimelineIcon>
-                                        <div className="flex flex-col md:flex-row gap-1">
-                                            <Typography variant="h7" color="blue-gray" className="text-sm md:text-base lg:text-lg">
-                                                {event.event}
-                                            </Typography>
-                                        </div>
-                                    </TimelineHeader>
-                                </TimelineItem>
-                            ))}
-                        </Timeline>
-                    </AccordionBody>
-                </Accordion>
-            ))} */}
-        </>
+        <table className="table table-hover course-list-table table-harassment">
+            <thead>
+                {data.Days.map((day, index) => (
+                    <React.Fragment key={index}>
+                        <tr>
+                            <th colSpan="2" style={{width: "5%", textAlign: "left", verticalAlign: "left"}}>
+                                <b>{day.Day}</b>
+                            </th>
+                        </tr>
+                        {day.Schedule.map((event, eventIndex) => (
+                            <tr key={eventIndex}>
+                                <td style={{textAlign: "left", verticalAlign: "left", width: "5%"}}>
+                                    {event.time}
+                                </td>
+                                <td style={{textAlign: "left", verticalAlign: "left", width: "15%"}}>
+                                    {event.event}
+                                </td>
+                            </tr>
+                        ))}
+                    </React.Fragment>
+                ))}
+            </thead>
+        </table>
     );
 }
