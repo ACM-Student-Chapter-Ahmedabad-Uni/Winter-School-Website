a2cb55df5433e03585d51b49aad6ce0317e76bd0
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-12-07 21:00:36 +0530

    update: Adding copyright over the card images.

--- src/components/cardsUI.jsx
+++ src/components/cardsUI.jsx
@@ -6,16 +6,19 @@ const TopicCards = ({ siteUrl }) => {
     return (
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
             {data.Topics.map((topic, index) => (
-                <div key={index} className="bg-white shadow-lg rounded-lg mx-6">
-                    <img className="w-full h-80 object-cover rounded-t-lg" src={topic.imageUrl} alt={topic.topic} />
+                <div key={index} className="bg-white shadow-lg rounded-lg mx-6 relative">
+                    <div className="w-full h-80 relative">
+                        <img className="w-full h-80 object-cover rounded-t-lg" src={topic.imageUrl} alt={topic.topic} />
+                        <p className="absolute bottom-0 right-0 bg-opacity-50 bg-black text-xs text-gray-500 p-1">Image provided by <a href="https://www.istockphoto.com/" target="_blank" rel="noopener noreferrer">iStock</a></p>
+                    </div>
                     <div className="p-6">
                         <h4 className="mb-3 text-xl font-semibold tracking-tight">{topic.topic}</h4>
-                        <p className="mb-2">Professor: <a className="no-underline" href={`${siteUrl}/speakers/#${topic.professor}`}>{topic.professor}</a></p>
+                        <p className="mb-2">Professor: <a className="no-underline" href={`/speakers/#${topic.professor}`}>{topic.professor}</a></p>
                         <p className="mb-2">Dates:
                             <ul>
                                 {topic.days.map((day, dayIndex) => (
                                     <li key={dayIndex}>
-                                        <a className="no-underline" href={`/schedule/#day${day}`}>{data_schedule["Days"][day - 1]["Day"]}</a>
+                                        <a className="no-underline" href={`/schedule/#day`}>{data_schedule["Days"][day - 1]["Day"]}</a>
                                     </li>
                                 ))}
                             </ul>
