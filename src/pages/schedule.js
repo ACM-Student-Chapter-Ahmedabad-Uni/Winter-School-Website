681489d925be6414779810a4d9295276327d1b32
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-12-10 21:43:38 +0530

    Update schedule.js

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -14,6 +14,7 @@ const SchedulePage = () => {
     <section>
       <div className="container px-4 py-16 mx-auto space-y-8 lg:max-w-3xl">
         <h2 className="text-2xl font-bold md:text-4xl text-center">Schedule</h2>
+        <h2 className="text-2xl font-bold md:text-2xl text-center">(Venue: School of Arts and Sciences, Room 004)</h2>
         <div className="space-y-8">
         {/* <iframe title="schedule" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRM0yU9FSljgQIi91x0QT2rTI8hufwAGDI6jAZYVEeW84pDymz-FGx-BF_h3uX8BcuXNH2w92AQBnx-/pubhtml?gid=1009124292&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="525" scrolling="no"></iframe> */}
         <AccordionCustomStyles/>
